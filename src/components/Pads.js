989279fc03661ac627deaaedeba7573c035c342d
JGPico <minq_2@hotmail.com>
2020-07-03 15:11:36 -0600

    messing with sound

--- src/components/Pads.js
+++ src/components/Pads.js
@@ -1,31 +1,47 @@
 import React from "react";
 
 export default function Pads() {
+  //   window.addEventListener("load", () => {
+  //     const sounds = document.querySelectorAll(".sound");
+  //     const pads = document.querySelectorAll(".pads div");
+
+  //     // Starting sounds
+  //     pads.forEach((pad, index) => {
+  //       pad.addEventListener("click", function () {});
+  //       sounds[index].play();
+  //     });
+  //   });
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log("button clicked");
+  };
+
   return (
     <div className="pads">
-      <div className="pad1">
-        <audio src="../sounds/bubbles.mp3"></audio>
-      </div>
+      <button onClick={handleClick} className="pad1">
+        <audio className="sound" src="../sounds/bubbles.mp3"></audio>
+      </button>
 
-      <div className="pad2">
-        <audio src="../sounds/clay.mp3"></audio>
-      </div>
+      <button className="pad2">
+        <audio className="sound" src="../sounds/clay.mp3"></audio>
+      </button>
 
-      <div className="pad3">
-        <audio src="../sounds/confetti.mp3"></audio>
-      </div>
+      <button className="pad3">
+        <audio className="sound" src="../sounds/confetti.mp3"></audio>
+      </button>
 
-      <div className="pad4">
-        <audio src="../sounds/glimmer.mp3"></audio>
-      </div>
+      <button className="pad4">
+        <audio className="sound" src="../sounds/glimmer.mp3"></audio>
+      </button>
 
-      <div className="pad5">
-        <audio src="../sounds/moon.mp3"></audio>
-      </div>
+      <button className="pad5">
+        <audio className="sound" src="../sounds/moon.mp3"></audio>
+      </button>
 
-      <div className="pad6">
-        <audio src="../sounds/ufo.mp3"></audio>
-      </div>
+      <button className="pad6">
+        <audio className="sound" src="../sounds/ufo.mp3"></audio>
+      </button>
     </div>
   );
 }
