c85846bffa59c6bfbfc819431cdf43f623a2928c
JGPico <minq_2@hotmail.com>
2020-07-03 11:53:58 -0600

    Added components

---
+++ src/components/Balls.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Balls() {
+  return <div>These will be music balls</div>;
+}
---
+++ src/components/Pads.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Pads() {
+  return (
+    <div className="pads">
+      <div className="pad1">
+        <audio src="../sounds/bubbles.mp3"></audio>
+      </div>
+
+      <div className="pad2">
+        <audio src="../sounds/clay.mp3"></audio>
+      </div>
+
+      <div className="pad3">
+        <audio src="../sounds/confetti.mp3"></audio>
+      </div>
+
+      <div className="pad4">
+        <audio src="../sounds/glimmer.mp3"></audio>
+      </div>
+
+      <div className="pad5">
+        <audio src="../sounds/moon.mp3"></audio>
+      </div>
+
+      <div className="pad6">
+        <audio src="../sounds/ufo.mp3"></audio>
+      </div>
+    </div>
+  );
+}
